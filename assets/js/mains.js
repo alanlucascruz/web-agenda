ff959e5921a523e562d162ea58ed66142ae615cb
Alan Cruz <alanlucascruz@gmail.com>
2023-11-15 22:52:43 -0300

    Stepper adicionado

---
+++ assets/js/mains.js
@@ -0,0 +1,13 @@
+// const splide = new Splide('.splide'),
+//   btnPagination = document.querySelector('#btn-pagination'),
+//   abas = {
+//     DadosIniciais: 0,
+//     DadosDataHora: 1,
+//     DadosUsuario: 2,
+//   };
+
+// btnPagination.addEventListener('click', () => {
+//   splide.go(abas.DadosDataHora);
+// });
+
+// splide.mount();
