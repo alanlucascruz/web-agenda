b4e2bd95ce97bac963681219f378999e92be0d02
Alan Cruz <alan.cruz@useall.com.br>
2023-11-20 13:21:40 -0300

    Implementação do Splide

--- assets/js/mains.js
+++ assets/js/mains.js
@@ -1,13 +1,17 @@
-// const splide = new Splide('.splide'),
-//   btnPagination = document.querySelector('#btn-pagination'),
-//   abas = {
-//     DadosIniciais: 0,
-//     DadosDataHora: 1,
-//     DadosUsuario: 2,
-//   };
+const splide = new Splide('#splide', {
+    drag: false,
+    arrows: false,
+    pagination: false,
+  }),
+  btnProximoAbaServico = document.querySelector('#btn-proximo-aba-servico'),
+  abas = {
+    DadosIniciais: 0,
+    DadosDataHora: 1,
+    DadosUsuario: 2,
+  };
 
-// btnPagination.addEventListener('click', () => {
-//   splide.go(abas.DadosDataHora);
-// });
+btnProximoAbaServico.addEventListener('click', () => {
+  splide.go(abas.DadosDataHora);
+});
 
-// splide.mount();
+splide.mount();
